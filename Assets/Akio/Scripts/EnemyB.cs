df54875cf9638cf97b9029dc82bb151ae9002b09
AkioMabuchi <mabuchiakio@gmail.com>
2020-02-01 15:40:32 +0900

    update

--- Assets/Akio/Scripts/EnemyB.cs
+++ Assets/Akio/Scripts/EnemyB.cs
@@ -2,17 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyB : MonoBehaviour
+public class EnemyB : Enemy
 {
+    GameObject gameObjectTarget;
+
     // Start is called before the first frame update
-    void Start()
+    protected override void Start()
     {
-        
+        base.Start();
+        gameObjectTarget = GameObject.Find("VirtualPlayer");
     }
 
     // Update is called once per frame
-    void Update()
+    protected override void Update()
     {
-        
+        base.Update();
+        float relativeX = gameObjectTarget.transform.position.x - transform.position.x;
+        float relativeY = gameObjectTarget.transform.position.y - transform.position.y;
+        float angle = Mathf.Atan2(relativeY, relativeX) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0.0f, 0.0f, angle);
     }
 }
