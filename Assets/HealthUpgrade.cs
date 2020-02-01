c33cbdaf5234fb0eb2937d413f3c9df0ee8c0519
arcase6 <41893575+arcase6@users.noreply.github.com>
2020-02-01 19:17:42 +0900

    Made item data
    Items and item data was added. Testing and further creation is still required.

---
+++ Assets/HealthUpgrade.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "HealthUpgrade", menuName = "Items/HealthUpgrade")]
+public class HealthUpgrade : ItemData
+{
+    public int amount;
+    public IntegerBindingVariable UnitMaxHealth;
+    protected override void DoItemAction()
+    {
+        UnitMaxHealth.Value += amount;
+    }
+}
---
+++ Assets/ShopItem.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ShopItem : MonoBehaviour, IPointerClickHandler
+{
+    public ItemData Item;
+    [HideInInspector]
+    public PlayerStats PlayerStats;
+    [HideInInspector]
+    public PlayerInventory PlayerInventory;
+
+
+    private Image DisplayPicture;
+    public IntegerMediator GearMediator;
+    public IntegerMediator ScrewMediator;
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (Item != null)
+        {
+            Item.UseItem();
+            if (Item.ConsumeOnUse)
+            {
+                this.Item = Item.NextItem;
+                if (Item == null)
+                    GameObject.Destroy(this.gameObject);
+                else
+                    InitializeDisplay();
+            }
+        }
+    }
+
+
+    private void OnEnable()
+    {
+        DisplayPicture = GetComponentInChildren<Image>();
+        InitializeDisplay();
+    }
+
+
+    private void InitializeDisplay()
+    {
+        DisplayPicture.sprite = Item.ItemImageSource;
+        GearMediator.Value = Item.GearCost;
+        ScrewMediator.Value = Item.ScrewCost;
+    }
+
+}
