a551747e3936c691390fde9e52d8b5b26ca67cc4
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2020-02-01 16:52:43 +0900

    Add bullet lifetime

--- Assets/Kevin Dev/Gun/Bullet.cs
+++ Assets/Kevin Dev/Gun/Bullet.cs
@@ -1,21 +1,26 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class Bullet : MonoBehaviour
-{
-    Rigidbody2D body;
-    public float speed = 8f;
-    Vector2 direction;
-    public void SetDirection(Vector2 direction) {
-        this.direction = direction;
-    }
+namespace GGJ2020 {
+    public class Bullet : MonoBehaviour {
+        Rigidbody2D body;
+        public float speed = 8f;
+        public float lifetime = 5f;
+        float shootTime;
+        Vector2 direction;
+        public void SetDirection(Vector2 direction) {
+            this.direction = direction;
+        }
 
-    void Start() {
-        body = GetComponent<Rigidbody2D>();
-    }
+        void Start() {
+            body = GetComponent<Rigidbody2D>();
+            shootTime = TimeManager.GameTime;
+        }
 
-    void Update() {
-        body.velocity = speed * direction.normalized;
+        void Update() {
+            body.velocity = speed * direction.normalized;
+            if (TimeManager.GameTime - shootTime >= lifetime) {
+                Destroy(gameObject);
+            }
+        }
     }
 }
--- Assets/Kevin Dev/Gun/Gun.cs
+++ Assets/Kevin Dev/Gun/Gun.cs
@@ -1,18 +1,17 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class Gun : MonoBehaviour
-{
-    public GameObject bulletPrefab;
-    public GameObject bulletOrigin;
-    // Start is called before the first frame update
+namespace GGJ2020 {
+    public class Gun : MonoBehaviour {
+        public GameObject bulletPrefab;
+        public GameObject bulletOrigin;
 
-    public void Use(Vector2 direction) {
-        transform.eulerAngles = new Vector3(0, 0, Vector2.SignedAngle(Vector2.up, direction));
-        GameObject bullet = GameObject.Instantiate(bulletPrefab, SpawnManager.parentTransform, true);
-        bullet.transform.position = bulletOrigin.transform.position;
-        bullet.layer = gameObject.layer; // Same collision layer as Gun
-        bullet.GetComponent<Bullet>().SetDirection(direction);
+        public void Use(Vector2 direction) {
+            transform.eulerAngles = new Vector3(0, 0, Vector2.SignedAngle(Vector2.up, direction));
+            GameObject bullet = GameObject.Instantiate(bulletPrefab, SpawnManager.parentTransform, true);
+            bullet.transform.position = bulletOrigin.transform.position;
+            bullet.layer = gameObject.layer; // Same collision layer as Gun
+            bullet.GetComponent<Bullet>().SetDirection(direction);
+        }
     }
+
 }
