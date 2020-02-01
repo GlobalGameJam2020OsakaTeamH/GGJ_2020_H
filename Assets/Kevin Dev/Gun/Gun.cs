572dcc01f3180bedead294c461bcffdbbca8f8cb
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2020-02-01 15:15:12 +0900

    Add global spawn parent

--- Assets/Kevin Dev/Gun/Gun.cs
+++ Assets/Kevin Dev/Gun/Gun.cs
@@ -5,13 +5,12 @@ using UnityEngine;
 public class Gun : MonoBehaviour
 {
     public GameObject bulletPrefab;
-    public GameObject bulletParent;
     public GameObject bulletOrigin;
     // Start is called before the first frame update
 
     public void Use(Vector2 direction) {
         transform.eulerAngles = new Vector3(0, 0, Vector2.SignedAngle(Vector2.up, direction));
-        GameObject bullet = GameObject.Instantiate(bulletPrefab, bulletParent.transform, true);
+        GameObject bullet = GameObject.Instantiate(bulletPrefab, SpawnParent.parentTransform, true);
         bullet.transform.position = bulletOrigin.transform.position;
         bullet.layer = gameObject.layer; // Same collision layer as Gun
         bullet.GetComponent<Bullet>().SetDirection(direction);
