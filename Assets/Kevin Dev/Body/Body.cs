9ef04d1cd39572bf24d099b905c7f3cb0e269fbc
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2020-02-01 17:26:30 +0900

    Only collide with collision handler

--- Assets/Kevin Dev/Body/Body.cs
+++ Assets/Kevin Dev/Body/Body.cs
@@ -12,7 +12,7 @@ namespace GGJ2020 {
         }
 
         void OnCollisionEnter2D(Collision2D collision) {
-            if (collision.gameObject.GetComponent<Bullet>()) {
+            if (collisionHandler != null && collision.gameObject.GetComponent<Bullet>()) {
                 collisionHandler.OnWeaponHit();
             }
         }
