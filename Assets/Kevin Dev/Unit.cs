0ea0508d75bbd8417a0c97feb2a9d4a7755ce6e6
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2020-02-01 13:41:19 +0900

    Movement

--- Assets/Kevin Dev/Unit.cs
+++ Assets/Kevin Dev/Unit.cs
@@ -1,11 +1,13 @@
 ﻿using UnityEngine;
 
 namespace GGJ2020 {
+    [RequireComponent(typeof(Rigidbody2D))]
     public class Unit : MonoBehaviour {
+        Rigidbody2D body;
+        Vector2 direction;
+        float speed = 4;
         public void SetDirection(Vector2 direction) {
-            if (direction.magnitude > 0) {
-                Debug.Log($"Direciton {direction}");
-            }
+            this.direction = direction;
         }
 
         public void UseSword() {
@@ -17,11 +19,11 @@ namespace GGJ2020 {
         }
 
         void Start() {
-
+            body = GetComponent<Rigidbody2D>();
         }
 
         void Update() {
-
+            body.velocity = direction * speed;
         }
     }
 }
