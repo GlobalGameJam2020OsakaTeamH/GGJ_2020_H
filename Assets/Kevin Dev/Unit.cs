42994b5ec6457be13de3750ea1c968f728222742
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2020-02-01 12:53:37 +0900

    Add Unit script

---
+++ Assets/Kevin Dev/Unit.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace GGJ2020 {
+    public class Unit : MonoBehaviour {
+        public void SetDirection(Vector2 direction) {
+            Debug.Log($"Direciton {direction}");
+        }
+
+        public void UseSword() {
+            Debug.Log($"Sword");
+        }
+
+        public void UseGun() {
+            Debug.Log($"Gun");
+        }
+
+        void Start() {
+
+        }
+
+        void Update() {
+
+        }
+    }
+}
