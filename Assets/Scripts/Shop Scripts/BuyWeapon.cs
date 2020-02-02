e79dd1c8fa83b983426e79730f1e753326bd2ce7
arcase6 <41893575+arcase6@users.noreply.github.com>
2020-02-02 13:06:57 +0900

    Improvments to UI look

---
+++ Assets/Scripts/Shop Scripts/BuyWeapon.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "HealthUpgrade", menuName = "Items/HealthUpgrade")]
+public class BuyWeapon : ItemData
+{
+    protected override void DoItemAction()
+    {
+ 
+    }
+}
