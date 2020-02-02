3a7a28bf36f8edbd7490dc9b18c42edd85981778
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2020-02-02 13:33:52 +0900

    Connect UI & Player

--- Assets/Kevin Dev/Player/PlayerHealth.cs
+++ Assets/Kevin Dev/Player/PlayerHealth.cs
@@ -3,7 +3,7 @@
 namespace GGJ2020 {
     public class PlayerHealth : MonoBehaviour, BodyCollisionHandler {
         public void OnWeaponHit(Collision2D collision) {
-            Debug.Log("Hit by enemy bullet");
+            GetComponent<PlayerStats>().HP -= 1;
         }
     }
 
