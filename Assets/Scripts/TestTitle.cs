02adc812e84c4e055cfc1fd0dba6905bdc7a784f
double-mod <52765153xuzhixin@gmail.com>
2020-02-02 14:26:12 +0900

    title done

--- Assets/Scripts/TestTitle.cs
+++ Assets/Scripts/TestTitle.cs
@@ -4,19 +4,57 @@ using UnityEngine;
 
 public class TestTitle : MonoBehaviour
 {
+    public float spd =0.2f;
+    public GameObject TitlePic;
+    public IntegerBindingVariable TowerHealth;
+    SpriteRenderer spriteRenderer;
+    SpriteRenderer titleRenderer;
+
+    float dt = 0.0f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        titleRenderer = TitlePic.GetComponent<SpriteRenderer>();
+            spriteRenderer.color = new Color(spriteRenderer.color.r, spriteRenderer.color.g, spriteRenderer.color.b, 0.0f);
+        Color temp = titleRenderer.color;
+        titleRenderer.color= new Color(temp.r,temp.g,temp.b, 0.0f);
+        TowerHealth.Value =100;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.Space))
+        if(spriteRenderer.color.a<1.0f)
         {
-            Manager.SceneMoveManager.Instance.Test = true;
-            Manager.MainGameManager.Instance.StartGame();
+            if(spriteRenderer.color.a > 0.5f)
+            {
+                if (titleRenderer.color.a < 1.0f)
+                {
+                    titleRenderer.color = new Color(titleRenderer.color.r, titleRenderer.color.g, titleRenderer.color.b, titleRenderer.color.a + Time.deltaTime * spd*2);
+                }
+            }
+            spriteRenderer.color = new Color(spriteRenderer.color.r, spriteRenderer.color.g, spriteRenderer.color.b, spriteRenderer.color.a+Time.deltaTime*spd);
+            if (TowerHealth.Value >0)
+            {
+                dt += Time.deltaTime;
+                Debug.Log(dt);
+                if(dt>0.1f)
+                {
+                    dt = 0;
+                    TowerHealth.Value -= 1;
+                }
+            }
         }
+        else
+        {
+            if (Input.GetKey(KeyCode.Space))
+            {
+
+                Manager.SceneMoveManager.Instance.Test = true;
+                Manager.MainGameManager.Instance.StartGame();
+            }
+        }
+
     }
 }
---
+++ Assets/Scripts/TowerAnimation.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerAnimation : MonoBehaviour
+{
+    public IntegerBindingVariable TowerHealth;
+    public Sprite[] sprites;
+
+
+    SpriteRenderer spriteRenderer;
+
+    public void SetEnergy(int _energy)
+    {
+        TowerHealth.Value = (int)_energy;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      //  sprites = new Sprite[7];
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (TowerHealth.Value < 14)
+        {
+            spriteRenderer.sprite = sprites[0];
+        }
+        else if (TowerHealth.Value < 28)
+        {
+            spriteRenderer.sprite = sprites[1];
+
+        }
+        else if (TowerHealth.Value < 42)
+        {
+            spriteRenderer.sprite = sprites[2];
+
+        }
+        else if (TowerHealth.Value < 56)
+        {
+            spriteRenderer.sprite = sprites[3];
+
+        }
+        else if (TowerHealth.Value < 70)
+        {
+            spriteRenderer.sprite = sprites[4];
+
+        }
+        else if (TowerHealth.Value < 84)
+        {
+            spriteRenderer.sprite = sprites[5];
+
+        }
+        else
+        {
+            spriteRenderer.sprite = sprites[6];
+
+        }
+    }
+}
