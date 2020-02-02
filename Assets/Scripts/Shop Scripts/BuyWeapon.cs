73bd058e00d619ecb75781ef7d6afa330f6506e3
arcase6 <41893575+arcase6@users.noreply.github.com>
2020-02-02 13:14:39 +0900

    Made UI prefabs

--- Assets/Scripts/Shop Scripts/BuyWeapon.cs
+++ Assets/Scripts/Shop Scripts/BuyWeapon.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "HealthUpgrade", menuName = "Items/HealthUpgrade")]
+[CreateAssetMenu(fileName = "BuyWeapon", menuName = "Items/BuyWeapon")]
 public class BuyWeapon : ItemData
 {
     protected override void DoItemAction()
