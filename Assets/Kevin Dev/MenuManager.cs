33a93db8aa42fad824c2c7a2929711c9caf397da
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2020-02-02 14:57:58 +0900

    Open menu with space when close

---
+++ Assets/Kevin Dev/MenuManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    public GameObject menu;
+    GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (PlayerIsAtBase && Input.GetKey(KeyCode.Space)) {
+            menu.SetActive(true);
+        }
+    }
+
+    bool PlayerIsAtBase {
+        get {
+            return (player.transform.position - transform.position).magnitude < 3;
+        }
+    }
+}
