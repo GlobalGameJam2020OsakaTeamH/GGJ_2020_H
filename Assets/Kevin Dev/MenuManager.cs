fad7e2184be5b930ce4826ca94d1ccb8e4cf5c96
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2020-02-03 13:31:31 +0900

    Stop time on menu

--- Assets/Kevin Dev/MenuManager.cs
+++ Assets/Kevin Dev/MenuManager.cs
@@ -1,26 +1,43 @@
 ﻿using UnityEngine;
 
-public class MenuManager : MonoBehaviour
-{
+public class MenuManager : MonoBehaviour {
     public GameObject menu;
     public GameObject menumessage;
     GameObject player;
-    // Start is called before the first frame update
-    void Start()
-    {
+
+    enum State {
+        NotAtBase,
+        AtBase,
+        MenuOpen
+    }
+
+    State state;
+
+    void Start() {
         player = GameObject.FindGameObjectWithTag("Player");
+        state = State.AtBase;
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        if (PlayerIsAtBase) {
-            menumessage.SetActive(true);
-            if (Input.GetKey(KeyCode.Space)) {
-                menu.SetActive(true);
-            }
-        } else {
-            menumessage.SetActive(false);
+    void Update() {
+        switch (state) {
+            case State.AtBase:
+                if (Input.GetButtonDown("Menu")) {
+                    OpenMenu();
+                }
+                if (!PlayerIsAtBase) {
+                    menumessage.SetActive(false);
+                    state = State.NotAtBase;
+                }
+                break;
+            case State.NotAtBase:
+                if (PlayerIsAtBase) {
+                    state = State.AtBase;
+                    menumessage.SetActive(true);
+                }
+                break;
+            case State.MenuOpen:
+                break;
         }
     }
 
@@ -28,5 +45,19 @@ public class MenuManager : MonoBehaviour
         get {
             return (player.transform.position - transform.position).magnitude < 3;
         }
+    }
+
+    public void OpenMenu() {
+        state = State.MenuOpen;
+        menumessage.SetActive(false);
+        menu.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    public void CloseMenu() {
+        state = State.AtBase;
+        menumessage.SetActive(true);
+        menu.SetActive(false);
+        Time.timeScale = 1;
     }
 }
