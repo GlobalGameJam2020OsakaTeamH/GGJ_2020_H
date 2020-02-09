cbaef164432ff821bef2cf29ea926323b665ebe4
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2020-02-10 00:08:39 +0900

    Fix and rewrite script

--- Assets/Akio/Scripts/DroppedPart.cs
+++ Assets/Akio/Scripts/DroppedPart.cs
@@ -1,15 +1,12 @@
 ﻿using UnityEngine;
 
-public class DroppedItem : MonoBehaviour
-{
+public class DroppedPart : MonoBehaviour {
     public Sprite screwSprite;
     public Sprite gearSprite;
 
     bool isScrew;
 
-    // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         isScrew = Random.Range(0, 2) == 1;
         if (isScrew) {
             GetComponent<SpriteRenderer>().sprite = screwSprite;
@@ -18,10 +15,8 @@ public class DroppedItem : MonoBehaviour
         }
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if(collision.gameObject.tag == "Player")
-        {
+    void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.gameObject.tag == "Player") {
             if (isScrew) {
                 collision.gameObject.GetComponent<PlayerInventory>().Screws += 1;
             } else {
